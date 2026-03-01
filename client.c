0f7b621470b389445a02aee439be12cebbce13f3
Beau Pullens <pullensbeau@gmail.com>
2026-03-01 15:23:42 +0100

    Implemented client

--- client.c
+++ client.c
@@ -39,6 +39,33 @@ int main (int argc, char * argv[])
     //      - send the request to the Req message queue
     //    until there are no more requests to send
     //  * close the message queue
+
+    char *req_queue_name = argv[1];
+    mqd_t req_queue = mq_open(req_queue_name, O_WRONLY);
     
+    while(true) {
+        int jobID, data, serviceID;
+
+        int result = getNextRequest(&jobID, &data, &serviceID);
+        if (result == NO_REQ) {
+            break;
+        }
+        if (result != NO_ERR) {
+            fprintf(stderr, "getNextRequest returned error %d\n", result);
+            break;
+        }
+
+        REQ_QUEUE msg;
+        msg.request_id = jobID;
+        msg.service_id = service_ID;
+        msg.data = data;
+
+        if (mq_send(req_queue, (const char*)&msg, sizeof(req_msg_t), 0) == -1) {
+            perror("mq_send Req_queue");
+            break;
+        }
+    }
+
+    mq_close(req_queue);
     return (0);
 }
